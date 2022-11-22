360fc1ab25f8c1ca4e99ad433c010e371f399cf0
Jinston Chung <jinstonchung@gmail.com>
2022-11-22 15:22:50 +0900

    disable-newtab-links

---
+++ disable-newtab-links.js
@@ -0,0 +1,13 @@
+(function() {
+    document.addEventListener('click', function(ev) {
+        var target = ev.target;
+        while ( target !== null ) {
+            if ( target.localName === 'a' && target.hasAttribute('target') ) {
+                ev.stopPropagation();
+                ev.preventDefault();
+                break;
+            }
+            target = target.parentNode;
+        }
+    });
+})();
